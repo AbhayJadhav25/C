a7e087d35ae8248d609bd6f3084bf5836ef66dad
Abhi-205 <abhayjadhav205@gmail.com>
2025-10-14 17:31:12 +0530

    Swap two numbers

---
+++ Swap.c
@@ -0,0 +1,15 @@
+// Swap two numbers
+#include <stdio.h>
+int main()
+{
+  int a, b;
+  printf("Enter any two numbers : ");
+  scanf("%d%d", &a, &b);
+
+  printf("Number before swap \n a = %d \t b = %d ", a, b);
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+
+  printf("\nNumber After swap \n a = %d \t b = %d ", a, b);
+}
